22ae2e27a4df6d1afec4934c79316865045d4cc6
GayeshmaWijerathna <109783810+GayeshmaWijerathna@users.noreply.github.com>
2023-10-09 18:59:43 +0530

    Upload Second part

---
+++ Spring_BackEnd/src/main/java/lk/ijse/spring/service/ItemService.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.ItemDTO;
+
+import java.util.List;
+
+public interface ItemService {
+
+    void saveItem(ItemDTO item);
+
+    void deleteItem(String code);
+
+    List<ItemDTO> getAllItem();
+
+    ItemDTO findItem(String code);
+
+    void updateItem(ItemDTO i);
+
+
+}
